d17053831c7ac36bf4385188002be4c0f382cec1
Henry Do <661034+vitaminh@users.noply.github.com>
2019-01-30 22:04:23 -0800

    Display player health field on UI

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInfo : MonoBehaviour
+{
+    // Initialize random number generator
+    System.Random rnd = new System.Random();
+
+    // Player info
+    public int playerHealth;
+
+    // UI Elements
+    public Text healthText;
+
+    void Start()
+    {
+        playerHealth = rnd.Next(1, 101);
+        setHealthText();
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Create text string to be displayed in UI
+    void setHealthText()
+    {
+        healthText.text = "Health: " + playerHealth.ToString();
+    }
+}
